17a7882dc00f92a282d55d50addc660aa82d52bc
Edgarcarrog <edgc21@hotmail.com>
2022-09-12 01:12:05 -0500

    cors fixed

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ const { urlencoded } = require("express");
 app.set("port", process.env.PORT || 4000);
 
 //middlewares
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cors());
 app.use(express.json());
 //app.use(urlencoded({ extended: true }));
