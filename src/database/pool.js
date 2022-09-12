9adae31b9ebc570913c8043f1fb1b5a737040d38
Edgarcarrog <edgc21@hotmail.com>
2022-09-12 00:34:35 -0500

    DB pool updated

--- src/database/pool.js
+++ src/database/pool.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   database: process.env.DB,
   password: process.env.DB_PASSWORD,
 });
