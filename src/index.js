70af339d0909678bb3176bf00742d94da479b5cd
Edgarcarrog <edgc21@hotmail.com>
2022-09-12 02:06:40 -0500

    Headers cors app-all

--- src/index.js
+++ src/index.js
@@ -15,14 +15,9 @@ app.set("port", process.env.PORT || 4000);
 
 //middlewares
 app.use(cors({ origin: "*" }));
-app.use((req, res, next) => {
+app.all("/", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
