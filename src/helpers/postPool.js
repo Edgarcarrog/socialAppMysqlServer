f3bef8bebf44b37dc4812ee687ea1b598b4a7395
Edgarcarrog <edgc21@hotmail.com>
2022-09-16 02:47:50 -0500

    getPosts date_public field deleted

--- src/helpers/postPool.js
+++ src/helpers/postPool.js
@@ -36,7 +36,7 @@ const getMyPosts = (userId) => {
 
 const getPosts = (followerId) => {
   const sql =
-    "SELECT p.Id, p.user, p.description, p.date, DATE_FORMAT(date, '%a %e %b %Y') AS date_public, (SELECT u.name FROM users u where u.userId = p.user) as name FROM posts p WHERE user in(SELECT followingId FROM follows WHERE followerId = ?) ORDER BY date DESC LIMIT 10";
+    "SELECT p.Id, p.user, p.description, p.date, (SELECT u.name FROM users u where u.userId = p.user) as name FROM posts p WHERE user in(SELECT followingId FROM follows WHERE followerId = ?) ORDER BY date DESC LIMIT 10";
 
   return promisePool
     .query("SET GLOBAL lc_time_names = 'es_MX'")
